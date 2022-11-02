bf050c147dfd494d4157d24c19c2c1f6f0c05568
junan <junan@junan.junan>
2022-11-02 06:22:39 -0400

    WPA2 Decrypt Success TODO: WPA2-PMF Decrypt

--- dot11decrypt.py
+++ dot11decrypt.py
@@ -9,15 +9,18 @@ def dot11i_decrypt(parser, TK):
         hexdump(pkt)
         dot11 = pkt[Dot11]
         ccmp = pkt[Dot11CCMP]
+        
         PN = "{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}".format(ccmp.PN5,ccmp.PN4,ccmp.PN3,ccmp.PN2,ccmp.PN1,ccmp.PN0)
         TA = dot11.addr2.replace(':','',5)
+        
         if pkt.haslayer(Dot11QoS):
             tid = '{:01x}'.format(pkt[Dot11QoS].TID)
         else:
             tid = '0'
-        priority = tid+'0'
-
+        priority = '0'+tid
+        
         nonce = bytes.fromhex(priority) + bytes.fromhex(TA) + bytes.fromhex(PN)
+        print(nonce)
         enc_cipher = AES.new(TK, AES.MODE_CCM, nonce, mac_len=8)
         # MIC는 제거
         decrypted_data = enc_cipher.decrypt(ccmp.data[:-8])
